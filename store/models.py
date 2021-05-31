73eddd20734707df9db359dac30dcaf4dd8269bc
gprok <gprok60@gmail.com>
2021-05-31 18:52:17 +0300

    added date to order

--- store/models.py
+++ store/models.py
@@ -39,7 +39,9 @@ class Items(models.Model):
 
 class Order(models.Model):
     email = models.CharField(max_length=120)
+    date_set = models.DateField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
